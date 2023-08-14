ea9ae380e79029df797b2276bf49e7b99a38a759
TerseTears <TerseTears@protonmail.com>
2023-08-14 17:15:46 +0330

    Add household ID durable value

--- parir/durable.py
+++ parir/durable.py
@@ -1,8 +1,7 @@
 import hbsir
-import yaml
 import pandas as pd
+import numpy as np
 from hbsir.metadata_reader import metadata, open_yaml
-from pdb import set_trace
 
 metadata.reload_file("tables")
 metadata.reload_file("schema")
@@ -19,13 +18,47 @@ def get_durable_df():
 
 
 durable_tbl = hbsir.load_table("durable", [1397], recreate=True, on_missing="create")
-durable_info = get_durable_df()
+durable_g2 = durable_tbl.view["eyn_durable4"].query("Durable_Group == 'G2'")
+# TODO Need to implement with deciles as well
+# TODO Weights seem to be different from Eynian...
+durable_value_average = (
+    durable_g2.query("Expenditure > 0")
+    .groupby("Code", as_index=False)
+    .apply(
+        lambda x: pd.Series(
+            {
+                "Count": len(x),
+                "Value_Average": np.average(x.Expenditure * 12, weights=x.Weight),
+            }
+        )
+    )
+)
+
+durable_info = (
+    pd.merge(get_durable_df(), durable_value_average, on="Code", how="left")
+    .assign(Value_Depreciated=lambda x: x.Value_Average * x.Depreciation / 100)
+    .rename(columns={"Name": "Durable_Good"})
+)
+
 durable_ownership = (
     pd.merge(durable_tbl[["ID", "Code"]], durable_info, on="Code", how="left")
-    .assign(Durable_Good=lambda x: x.Name.astype("category"))
+    .assign(Durable_Good=lambda x: x["Durable_Good"].astype("category"))
     .groupby(["ID", "Durable_Good"], dropna=False, as_index=False)
     .size()
     .assign(Owns=lambda x: x["size"] > 0)
     .drop(columns="size")
 )
-durable_ownership
+durable_ownership = pd.merge(
+    durable_ownership,
+    durable_info[["Durable_Good", "Value_Average", "Value_Depreciated"]],
+    on="Durable_Good",
+    how="left",
+)
+durable_ownership.groupby("ID").apply(
+    lambda x: pd.Series(
+        {
+            "Value_Average": np.sum(x.Value_Average * x.Owns),
+            "Value_Depreciated": np.sum(x.Value_Depreciated * x.Owns) / 12,
+        }
+    )
+)
